6a9b231d9315550f1c044e5a41ccb517cf2d5ab4
elratondesusi <37693098+elratondesusi@users.noreply.github.com>
2019-12-11 21:00:03 +0100

    Create Record.py

---
+++ Record.py
@@ -0,0 +1,8 @@
+class Record:
+    def __init__(self, id, lended, id_company):
+        self.id = id
+        self.lended = lended
+        self.ECV = ECV
+        self.id_company = id_company ##kto naozaj zaparkoval
+    def save(self):
+        pass
